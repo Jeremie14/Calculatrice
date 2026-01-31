352ee1e41b7a2941894c37fd143e016fc2d5ab61
Jeremie <jerpelletier13@icloud.com>
2026-01-30 23:32:46 -0500

    Avancement de l'interface

--- CalculatricE/src/App.java
+++ CalculatricE/src/App.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class App 
@@ -5,7 +7,13 @@ public class App
     public static void main(String[] args)
     {
         JFrame frame = new JFrame();
+        frame.setTitle("Calculatrice");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 420);
         frame.setVisible(true);
+
+        ImageIcon image = new ImageIcon("calculatrice.png");
+        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(new Color(160, 160, 160));
     }
 }
