2ddef64893385235115604ef3bcac1dd32ab83f2
Jeremie <jerpelletier13@icloud.com>
2026-01-30 23:08:40 -0500

    Création de l'interface

--- CalculatricE/src/App.java
+++ CalculatricE/src/App.java
@@ -1,5 +1,11 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.JFrame;
+
+public class App 
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(420, 420);
+        frame.setVisible(true);
     }
 }
