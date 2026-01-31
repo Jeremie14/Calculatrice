71c0f2d6b102066901575b31f0d1d472a675fc8d
Jeremie <jerpelletier13@icloud.com>
2026-01-31 13:57:40 -0500

    Création du premier bouton

--- CalculatricE/src/App.java
+++ CalculatricE/src/App.java
@@ -1,19 +1,24 @@
 import java.awt.Color;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class App 
 {
     public static void main(String[] args)
     {
+        JButton button = new JButton();
+        button.setBounds(50, 50, 100, 50);
         JFrame frame = new JFrame();
         frame.setTitle("Calculatrice");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 420);
+        frame.setLayout(null);
+        frame.setSize(500, 500);
         frame.setVisible(true);
+        frame.add(button);
 
         ImageIcon image = new ImageIcon("calculatrice.png");
         frame.setIconImage(image.getImage());
-        frame.getContentPane().setBackground(new Color(160, 160, 160));
+        frame.getContentPane().setBackground(new Color(160, 160, 160)); // rgb color
     }
 }
